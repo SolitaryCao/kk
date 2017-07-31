c5689611d681dbe63c6a3f95e9d93a2d24753e9b
SolitaryCao <shangdishengkui@outlook.com>
2017-07-31 09:55:30 +0800

    Update kk.sh

--- kk.sh
+++ kk.sh
@@ -2,5 +2,6 @@
 curl $1 |awk -v head="playurl\":\"" -v tail="&from" '{print substr($0, index($0,head)+length(head),index($0,tail)-index($0,head)-length(head))}'|sed -n '/./p'  >>1.txt
 curl $1 |awk -v head="play_name\">" -v tail="</h2>" '{print substr($0, index($0,head)+length(head),index($0,tail)-index($0,head)-length(head))}'|sed -n '/./p' >>2.txt
 url=$(cat 1.txt)
-name=$(cat 2.txt)".m4a"
-wget   -O "1.m4a" $url
+name=$(cat 2.txt)
+wget   -O "$name.m4a" $url
+rm -f 1.txt 2.txt
