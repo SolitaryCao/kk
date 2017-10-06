e10b2a8bb9785b75aef3673cb508c70dea521bdc
SolitaryCao <shangdishengkui@outlook.com>
2017-10-06 22:26:54 +0800

    Update dlAll.sh

--- dlAll.sh
+++ dlAll.sh
@@ -1,6 +1,13 @@
 #/bin/bash
+name=`curl $1 | grep my_show__name| awk -F ">" '{ print $2}'| awk -F "<" '{print $1}' `
+mkdir $name
+cp dl.sh $name 
+cd $name
 curl $1 | grep "=personal" | awk -F"\"" '{print $2}'|cat |while read LINE 
 do 
 	./dl.sh $LINE
 done
+cd ../
+tar -zcf $name.tar.gz $name
+rm -rf $name 
 exit 0
